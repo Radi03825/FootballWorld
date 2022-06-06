0f06318390baf3ac2e504fee3a7b242774177a2f
Radi <radi038425@gmail.com>
2022-06-06 10:20:15 +0300

    Initial commit

---
+++ src/test/java/bg/softuni/FootballWorld/FootballWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.FootballWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
