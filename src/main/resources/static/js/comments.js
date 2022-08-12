6feaff7c379f3dac4ea2921a0f34a57dc795e1ba
Radi <radi03825@gmail.com>
2022-08-12 16:16:36 +0300

    Change links

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -15,7 +15,7 @@ async function handleFormSubmission(event) {
 
     if (messageVal.trim() !== '') {
 
-        fetch(`http://localhost:8080/api/${playerId}/comments`, {
+        fetch(`https://footballlworld.herokuapp.com//api/${playerId}/comments`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -48,7 +48,7 @@ function commentAsHtml(comment) {
     return commentHtml
 }
 
-fetch(`http://localhost:8080/api/${playerId}/comments`, {
+fetch(`https://footballlworld.herokuapp.com//api/${playerId}/comments`, {
     headers: {
         "Accept": "application/json"
     }
